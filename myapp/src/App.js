7384f92152242b23df45e1c96e1bc0452395c700
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-09-06 18:30:48 +0100

    finishing components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,21 @@
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import MyProjects from "./components/MyProjects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
     <NavBar />
+    <Home />
+    <About />
+    <MyProjects />
+    <Experience />
+    <Contact />
+    <SocialLinks />
     </div>
   );
 }
