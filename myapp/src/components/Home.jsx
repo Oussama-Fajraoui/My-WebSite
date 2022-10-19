20bbfa722353ee54d44a695461e01a079aad30fa
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-19 22:30:49 +0100

    init

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           name="home"
           className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800 "
         >
-          <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+          <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-10 md:flex-row">
             <div >
               <p className='text-white'>Hi, my name is :</p>
               <h2 className="text-4xl sm:text-3xl font-bold text-white">
