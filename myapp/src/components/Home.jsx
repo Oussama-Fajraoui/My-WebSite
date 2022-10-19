8ba83e64c2dae9304a008f26017f9cc80c5cdd73
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-19 22:49:20 +0100

    init

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800 "
         >
           <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-10 md:flex-row">
-            <div className='mt-20'>
+            <div className='mt-40'>
               <p className='text-white'>Hi, my name is :</p>
               <h2 className="text-4xl sm:text-3xl font-bold text-white">
                 Oussama Fajraoui
@@ -36,7 +36,7 @@ knowledge skills.
                 >
                   My Projects
                   <span className="group-hover:rotate-90 duration-300">
-                    <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                    <MdOutlineKeyboardArrowRight size={10} />
                   </span>
                 </Link>
               </div>
@@ -46,7 +46,7 @@ knowledge skills.
               <img
                 src={HeroImage}
                 alt="my profile"
-                className="rounded-2xl mx-auto w-2/3"
+                className="rounded-2xl mx-auto w-2/3 mt-40 outline-hidden"
               />
             </div>
           </div>
